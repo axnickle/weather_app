e49d3408652ef00dd8e3b5f59c12fa38e00b23f5
axnickle <onlychangethatmakescents@gmail.com>
2020-06-22 13:24:00 -0400

    parse current day weather data

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,11 @@
 class SearchController < ApplicationController
+  include WeatherData
 
   def index
     if params['location']
       coordinates = Geocoder.search(params['location']).first.coordinates
       @response = Openweather::Search.one_call(coordinates)
+      @current = current_time_data
     end
   end
 end
--- lib/weather_data.rb
+++ lib/weather_data.rb
@@ -2,4 +2,28 @@ module WeatherData
   def data
     @data = JSON.parse @response.body
   end
+
+  def current_data
+    data['current']
+  end
+
+  def current_time_data
+    {'current_temp' => convert_temp, 'description' => weather_description, 'icon' => weather_icon }
+  end
+
+  def convert_temp
+    (((current_data['temp'].to_d) - 273.15) * 9/5 + 32).to_i
+  end
+
+  def weather_description
+    (current_data['weather'].first)['description']
+  end
+
+  def weather_icon
+    icon = (current_data['weather'].first)['icon']
+    "http://openweathermap.org/img/w/#{icon}.png"
+  end
 end
+
+# last line in ruby get returned
+# Rails.logger.error("******** #{current_data['temp'].to_d}")
